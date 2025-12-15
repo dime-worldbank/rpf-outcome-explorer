cc7ac888415a156a3525b430a81732423542f6bf
Yukiko Suzuki <yukinko.iwasaki@gmail.com>
2025-12-15 14:27:31 -0500

    Update contentText.jsx

--- frontend/src/components/contentText.jsx
+++ frontend/src/components/contentText.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 
-const COUNTRIES = ["Rwanda"]
+const COUNTRIES = ["Rwanda", "Tanzania", "Uganda", "Kenya", "Ethiopia", "Ghana", "Nigeria", "South Africa", "India", "Bangladesh", "Pakistan", "Nepal", "Myanmar", "Vietnam", "Viet Nam", "Philippines", "Indonesia", "Colombia", "Peru", "Mexico", "Brazil", "Argentina", "Liberia", "Sierra Leone", "Mozambique", "Malawi", "Cambodia", "Laos", "Thailand", "Sri Lanka", "Egypt", "Morocco", "Tunisia", "Algeria", "Jordan", "Lebanon", "Iraq", "Afghanistan", "Yemen", "Honduras", "Guatemala", "El Salvador", "Nicaragua", "Côte d'Ivoire", "Senegal", "Mali", "Burkina Faso", "Chad", "Niger", "Sudan", "South Sudan", "Albania", "North Macedonia", "Serbia", "Bosnia and Herzegovina", "Montenegro", "Kosovo", "Ukraine", "Moldova", "Georgia", "Armenia", "Azerbaijan", "Belarus", "Russia", "China", "Turkey", "Saudi Arabia", "United Arab Emirates", "Qatar", "Kuwait", "Oman", "Bahrain", "Iceland", "Norway", "Sweden", "Finland", "Denmark", "Netherlands", "Belgium", "Luxembourg", "Germany", "France", "Switzerland", "Austria", "Italy", "Spain", "Portugal", "Greece", "Czech Republic", "Slovakia", "Hungary", "Poland", "Lithuania", "Latvia", "Estonia", "Kenya" , "Canada", "United States", "United Kingdom", "Ireland", "Australia", "New Zealand", 'Angola', 'Botswana', 'Cameroon', 'Congo', 'Democratic Republic of the Congo', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', "DRC", 'Tajikistan', 'Turkmenistan', 'Uzbekistan', 'Zambia', 'Zimbabwe', 'Trinidad and Tobago'];
 const anonymizeCountry = (text) => {
-    COUNTRIES.forEach(country => {
+    COUNTfRIES.forEach(country => {
         const regex = new RegExp(`\\b${country}\\b`, 'gi');
         text = text.replace(regex, "[REDACTED]");
     });
